69dca3303027682b8292b75fa2ab041725c556f9
xployt404 <101953958+xployt404@users.noreply.github.com>
2022-04-03 21:04:44 +0000

    Add files via upload

---
+++ serial.ino
@@ -0,0 +1,29 @@
+// Wire Slave Receiver
+
+#include <Wire.h>
+#include "Keyboard.h"
+
+void setup() {
+  // Join I2C bus as slave with address 4
+  Wire.begin(0x20);
+  Keyboard.begin();
+  // Call receiveEvent when data received                
+  Wire.onReceive(receiveEvent);
+}
+ 
+// Function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+  while (Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Keyboard.print(c);
+    if (c == 1) {
+      Keyboard.print("c");
+        
+    }else{
+      Keyboard.print("none");
+      }
+  }
+}
+void loop() {
+  delay(100);
+}
