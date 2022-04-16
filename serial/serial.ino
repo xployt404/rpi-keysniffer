184996c19badb35eace96167bbf5ab783da14ba4
xployt404 <101953958+xployt404@users.noreply.github.com>
2022-04-16 19:06:21 +0000

    faster receive and send
    also more simple code

--- serial/serial.ino
+++ serial/serial.ino
@@ -1,8 +1,42 @@
 // Wire Slave Receiver
-
+#include <ArduinoSTL.h>
+#include <map>
 #include <Wire.h>
 #include "Keyboard.h"
+#include <string>
+using namespace std;
 
+// function that is called when handling a modifier key
+uint8_t modifiers(char c){
+  // modifier dictionary (called map in c++)
+  std::map<char, uint8_t> mapModifiers = {
+    {1,   KEY_RIGHT_ALT},
+    {2,    KEY_LEFT_ALT},
+    {3,   KEY_LEFT_CTRL},
+    {4, KEY_RIGHT_SHIFT}
+  };
+  return mapModifiers[c];
+}
+
+// function that is called when handling a special key
+uint8_t specialKeys(char c){
+  // special key dictionary (called map in c++)
+  std::map<char, uint8_t> mapSpecial = {
+    {17,   KEY_LEFT_ARROW},
+    {18,  KEY_RIGHT_ARROW},
+    {19,     KEY_UP_ARROW},
+    {20,   KEY_DOWN_ARROW},
+    {8,     KEY_BACKSPACE},
+    {6,        KEY_DELETE},
+    {13,       KEY_RETURN},
+    {32,               32},
+    {7,     KEY_CAPS_LOCK},
+    {9,           KEY_TAB}
+  };
+  return mapSpecial[c];
+}
+
+// STARTING CONFIGURATION
 void setup() {
   // Join I2C bus as slave with address 4
   Wire.begin(0x20);
@@ -15,89 +49,18 @@ void setup() {
 void receiveEvent(int howMany) {
   while (Wire.available()) {
     char c = Wire.read(); // receive byte as a character
-
-    // SPACE
-    if (c==32) {
-      Keyboard.print(" ");
-
-    // ENTER
-    }else if (c==13){
-      Keyboard.write(KEY_RETURN);
+    if (c > 32) {
+      Keyboard.print(c);
       Keyboard.releaseAll();
-
-    // SHIFT
-    }else if(c==15){
-      Keyboard.press(KEY_RIGHT_SHIFT);
-
-    // ALT
-    }else if (c==2) {
-      Keyboard.press(KEY_LEFT_ALT);
-
-    }else if (c==3) {
-      Keyboard.press(KEY_RIGHT_CTRL);
-
-    // ALT GR
-    }else if (c==1) {
-      Keyboard.press(KEY_RIGHT_ALT);
-
-    // BACKSPACE
-    }else if (c==8) {
-      Keyboard.write(KEY_BACKSPACE);
+    }else if (c < 5) {
+      Keyboard.press(modifiers(c));
+    }else {
+      Keyboard.press(specialKeys(c));
       Keyboard.releaseAll();
-
-    // TAB
-    }else if(c==9){
-      Keyboard.write(KEY_TAB);
-
-    // CAPS LOCK
-    }else if (c==178) {
-      Keyboard.press(KEY_CAPS_LOCK);
-      Keyboard.releaseAll();
-    // DELETE
-    }else if (c==127) {
-      Keyboard.write(KEY_DELETE);
-
-    // Ö
-    }else if (c==246) {
-      Keyboard.press(148);
-      Keyboard.releaseAll();
-
-    // Y
-    }else if (c==122) {
-      Keyboard.print("y");
-      Keyboard.releaseAll();
-      
-    // X
-    }else if (c==121) {
-      Keyboard.print("z");
-      Keyboard.releaseAll();
-
-    // LEFT_ARROW
-    }else if (c==17) {
-      Keyboard.press(KEY_LEFT_ARROW);
-      Keyboard.releaseAll();
-
-    // RIGHT_ARROW
-    }else if (c==18) {
-      Keyboard.press(KEY_RIGHT_ARROW);
-      Keyboard.releaseAll();
-
-    // UP_ARROW
-    }else if (c==19) {
-      Keyboard.press(KEY_UP_ARROW);
-      Keyboard.releaseAll();
-
-    // DOWN_ARROW
-    }else if (c==20) {
-      Keyboard.press(KEY_DOWN_ARROW);
-      Keyboard.releaseAll();
-
-    }else{
-        Keyboard.print(c);
-        Keyboard.releaseAll();
     }
   }
 }
+// USELESS LOOP
 void loop() {
-  delay(1);
+  delay(10);
 }
